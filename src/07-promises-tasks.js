ca6365bf3db223ef98ace283dd3bb7c7cd2ae9c6
Valeria <Valeryia.Karzhaneuskaya@mail.ru>
2020-05-14 23:28:15 +0300

    feat: add solutions to promises tasks

--- src/07-promises-tasks.js
+++ src/07-promises-tasks.js
@@ -56,8 +56,8 @@ function willYouMarryMe(isPositiveAnswer) {
  *    })
  *
  */
-function processAllPromises(/* array */) {
-  throw new Error('Not implemented');
+function processAllPromises(array) {
+  return Promise.all(array);
 }
 
 /**
@@ -79,8 +79,8 @@ function processAllPromises(/* array */) {
  *    })
  *
  */
-function getFastestPromise(/* array */) {
-  throw new Error('Not implemented');
+function getFastestPromise(array) {
+  return Promise.race(array);
 }
 
 /**
@@ -100,8 +100,15 @@ function getFastestPromise(/* array */) {
  *    });
  *
  */
-function chainPromises(/* array, action */) {
-  throw new Error('Not implemented');
+function chainPromises(array, action) {
+  return new Promise(((resolve) => {
+    const results = [];
+    array.forEach((element) => {
+      element.then((result) => results.push(result))
+        .catch((err) => err);
+    });
+    resolve(results);
+  })).then((results) => results.reduce(action));
 }
 
 module.exports = {
